c943c2c7dd7c1bd3025de121df2e9979b367c3c9
Davis Tyler <127260497+sivad360@users.noreply.github.com>
2024-03-15 16:54:23 -0700

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,8 @@
 //Name: Davis Tyler
 //Class: CS145
 //Date: 2/6/2024
+//file: Test
+//Resources: cs 145 Lectures
 //Program: this program promps generates a word search from user inputted words. The menu will
 // prompt users to choose to generate a new word search, print a formatted word search, or print
 // a formatted solution to their word search. 
